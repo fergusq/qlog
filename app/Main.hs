44a9c66b6198ec9909885615a8a42d9ca9b820fb
Iikka Hauhio <iikka@kaivos.org>
2018-11-08 14:27:46 +0200

    new sum example

--- app/Main.hs
+++ app/Main.hs
@@ -9,12 +9,15 @@ main = output $ sampleQuery (State {substitutions=M.empty, counter=0})
 
 output [] = putStrLn "Ei."
 output (state@State { substitutions = ss }:n) = do putStrLn "Kyllä."
-                                                   forM_ (M.toList ss) $ \(v, e) -> putStrLn (v ++ " = " ++ show (deepWalk state e))
+                                                   putStrLn $ "X = " ++ show (deepWalk state (Variable 1))
                                                    getLine
                                                    output n
 
+infixr 9 //
 (//) = Compound
 
-sampleQuery = callFresh nat
+sampleQuery = callFresh nat --callFresh $ \x -> summa ("s"//["s"//["0"//[]]]) ("s"//["s"//["0"//[]]]) x
 
-nat x = (x #= ("0"//[])) #|# (callFresh $ \y -> nat y #&# (x #= ("s"//[y])))
+summa x y s = (x #= "0"//[] #&# y #= s) #|# callFresh (\z -> x #= "s"//[z] #&# summa z ("s"//[y]) s)
+
+nat x = x #= "0"//[] #|# callFresh (\y -> nat y #&# x #= "s"//[y])
