59ff8cbea9a33e680b99c2a040e1f43113abe65b
Iikka Hauhio <iikka@kaivos.org>
2018-11-08 21:21:51 +0200

    evaling expressions

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,8 @@ import qualified Data.Map as M
 import Lib
 
 main :: IO ()
-main = output $ sampleQuery (State {substitutions=M.empty, counter=0})
+main = do print natF
+          output $ sampleQuery (State {substitutions=M.empty, counter=0})
 
 output [] = putStrLn "Ei."
 output (state@State { substitutions = ss }:n) = do putStrLn "Kyllä."
@@ -16,8 +17,12 @@ output (state@State { substitutions = ss }:n) = do putStrLn "Kyllä."
 infixr 9 //
 (//) = Compound
 
-sampleQuery = callFresh nat --callFresh $ \x -> summa ("s"//["s"//["0"//[]]]) ("s"//["s"//["0"//[]]]) x
+sampleQuery = eval (M.fromList natF) [] [] ("nat"//[Variable 1])
+--sampleQuery = callFresh nat
+--sampleQuery = callFresh $ \x -> summa ("s"//["s"//["0"//[]]]) ("s"//["s"//["0"//[]]]) x
 
 summa x y s = (x #= "0"//[] #&# y #= s) #|# callFresh (\z -> x #= "s"//[z] #&# summa z ("s"//[y]) s)
 
-nat x = x #= "0"//[] #|# callFresh (\y -> nat y #&# x #= "s"//[y])
+nat x = callFresh $ \y -> (x #= "0"//[]) #|# (nat y #&# x #= "s"//[y])
+
+natF = [("nat", (";"//["="//[Variable 0, "0"//[]], ","//["nat"//[Variable 1], "="//[Variable 0, "s"//[Variable 1]]]], [0]))]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,13 @@
 module Lib where
 import Control.Applicative
+import Data.Char (isAlphaNum)
 import Data.List
 import Data.Maybe
 import qualified Data.Map as M
+
+import Debug.Trace
+
+-- Expressions
 
 data Expr = Variable Int
           | Compound String [Expr]
@@ -11,11 +16,16 @@ data Expr = Variable Int
 instance Show Expr where
   show (Variable v) = "_" ++ show v
   show (Compound f []) = f
+  show (Compound f [a, b])
+    | all (not . isAlphaNum) f = "(" ++ show a ++ " " ++ f ++ " " ++ show b ++ ")"
+    | otherwise                = f ++ "(" ++ show a ++ ", " ++ show b ++ ")"
   show (Compound f as) = f ++ "(" ++ intercalate ", " (map show as) ++ ")"
+
+-- State
 
 type SMap = M.Map Int Expr
 
-data State = State { substitutions :: SMap, counter :: Int }
+data State = State { substitutions :: SMap, counter :: Int } deriving Show
 
 addSubstitution :: Int -> Expr -> State -> State
 addSubstitution v x state = state { substitutions = M.insert v x (substitutions state) }
@@ -23,6 +33,8 @@ addSubstitution v x state = state { substitutions = M.insert v x (substitutions 
 substitute :: Int -> Expr -> Expr -> Expr
 substitute v r expr@(Variable u) = if v == u then r else expr
 substitute v r (Compound f as) = Compound f $ map (substitute v r) as
+
+-- Goal
 
 type Goal = State -> [State]
 
@@ -34,6 +46,8 @@ deepWalk :: State -> Expr -> Expr
 deepWalk state x@(Variable v)    = fromMaybe x $ deepWalk state <$> M.lookup v (substitutions state)
 deepWalk state x@(Compound f as) = Compound f $ map (deepWalk state) as
 deepWalk _     x                 = x
+
+-- Unification
 
 unify :: Expr -> Expr -> Goal
 unify a b state = let a' = walk state a
@@ -53,26 +67,65 @@ unifyAll (_:_) [] _ = []
 unifyAll [] (_:_) _ = []
 unifyAll (x:xs) (y:ys) state = unify x y state >>= unifyAll xs ys
 
+-- Conjunction and disjunction
+
+disj :: Goal -> Goal -> Goal
+disj x y state = x state <|> y state
+
+conj :: Goal -> Goal -> Goal
+conj x y state = x state >>= y
+
+-- Operator aliases
+
 infixr 4 #=
-
-(#=) = unify
-
 infixl 3 #|#
 infixl 3 #&#
 
-(#|#) :: Goal -> Goal -> Goal
-(#|#) x y state = x state <|> y state
+(#=) = unify
+(#|#) = disj
+(#&#) = conj
 
-(#&#) :: Goal -> Goal -> Goal
-(#&#) x y state = x state >>= y
+-- callFresh and fresh
 
 callFresh :: (Expr -> Goal) -> Goal
 callFresh f state = let c = counter state + 1
                         state' = state { counter = c }
                     in f (Variable c) state'
 
-fresh vs = fresh' vs []
+fresh :: Int -> ([Expr] -> Goal) -> Goal
+fresh n = fresh' n []
 
 fresh' :: Int -> [Expr] -> ([Expr] -> Goal) -> Goal
 fresh' 0 es f = f es
 fresh' n es f = callFresh $ \e -> fresh' (n-1) (e:es) f
+
+-- Converting expressions to predicate functions
+
+eval :: M.Map String (Expr, [Int]) -> [Int] -> [Expr] -> Expr -> Goal
+--eval fs ps as e state | trace ("fs="++show fs++" ps="++show ps++" as="++show as++" e="++show e++" state="++show state) False = undefined
+eval fs ps as e
+  | null vs   = eval' fs ps [] as e
+  | otherwise = fresh (length vs) (\vs' -> eval' fs ps (zip vs vs') as e)
+  where vs = nub $ searchVars ps e
+
+searchVars :: [Int] -> Expr -> [Int]
+searchVars ps (Variable i)
+  | i `elem` ps = []
+  | otherwise   = [i]
+searchVars ps (Compound f as) = concatMap (searchVars ps) as
+
+eval' :: M.Map String (Expr, [Int]) -> [Int] -> [(Int, Expr)] -> [Expr] -> Expr -> Goal
+--eval' fs ps vs as e | trace ("' fs="++show fs++" ps="++show ps++" vs="++show vs++" as="++show as++" e="++show e) False = undefined
+eval' fs _  _  _  (Variable _)          = error "expected predicate"
+eval' fs ps vs as (Compound ";" [a, b]) = disj (eval' fs ps vs as a) (eval' fs ps vs as b)
+eval' fs ps vs as (Compound "," [a, b]) = conj (eval' fs ps vs as a) (eval' fs ps vs as b)
+eval' fs ps vs as (Compound "=" [a, b]) = unify (evalExpr ps vs as a) (evalExpr ps vs as b)
+eval' fs ps vs as (Compound f   args)   = case M.lookup f fs of
+                                            Just (body, parameters) -> eval fs parameters (map (evalExpr ps vs as) args) body
+                                            Nothing -> error ("undefined predicate "++f)
+
+evalExpr :: [Int] -> [(Int, Expr)] -> [Expr] -> Expr -> Expr
+evalExpr ps vs as (Variable v)    = case elemIndex v ps of
+                                      Just i  -> as !! i
+                                      Nothing -> fromMaybe (error "undefined variable") $ lookup v vs
+evalExpr ps vs as (Compound f es) = Compound f (map (evalExpr ps vs as) es)
