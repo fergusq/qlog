05a97e003aa511fbca16ccc0bb348a7fcc11e7e0
Iikka Hauhio <iikka@kaivos.org>
2018-11-08 21:24:36 +0200

    improved output

--- app/Main.hs
+++ app/Main.hs
@@ -6,20 +6,26 @@ import Lib
 
 main :: IO ()
 main = do print natF
-          output $ sampleQuery (State {substitutions=M.empty, counter=0})
+          let (vars, goal) = sampleQuery
+          output vars $ goal (State {substitutions=M.empty, counter=0})
 
-output [] = putStrLn "Ei."
-output (state@State { substitutions = ss }:n) = do putStrLn "Kyllä."
-                                                   putStrLn $ "X = " ++ show (deepWalk state (Variable 1))
-                                                   getLine
-                                                   output n
+output :: [Expr] -> [State] -> IO ()
+output vars [] = putStrLn "Ei."
+output vars states = putStrLn "Kyllä." >> output' vars states
+
+output' :: [Expr] -> [State] -> IO ()
+output' vars [] = putStrLn "."
+output' vars (state@State { substitutions = ss }:n) = do forM_ (zip vars ['X', 'Y', 'Z']) $ \(e, v) ->
+                                                           putStrLn $ (v:" = ") ++ show (deepWalk state e)
+                                                         getLine
+                                                         output' vars n
 
 infixr 9 //
 (//) = Compound
 
-sampleQuery = eval (M.fromList natF) [] [] ("nat"//[Variable 1])
---sampleQuery = callFresh nat
---sampleQuery = callFresh $ \x -> summa ("s"//["s"//["0"//[]]]) ("s"//["s"//["0"//[]]]) x
+sampleQuery = ([Variable 1], eval (M.fromList natF) [] [] ("nat"//[Variable 1]))
+--sampleQuery = ([Variable 1], callFresh nat)
+--sampleQuery = ([Variable 1], callFresh $ \x -> summa ("s"//["s"//["0"//[]]]) ("s"//["s"//["0"//[]]]) x)
 
 summa x y s = (x #= "0"//[] #&# y #= s) #|# callFresh (\z -> x #= "s"//[z] #&# summa z ("s"//[y]) s)
 
